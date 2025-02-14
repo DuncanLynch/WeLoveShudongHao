4d2cb9d8ca5596d70e1aa1c9f4e5b96356005292
HypaDeficit <duncan@thelynches.com>
2025-02-14 13:33:29 -0500

    fsdgfghghfdghghfxghfhg

--- src/components/RevealButton.jsx
+++ src/components/RevealButton.jsx
@@ -3,7 +3,7 @@
 export default function RevealButton({children, handleClick})
 {
     return (
-        <button onClick={handleClick} className="rounded-3xl bg-red-300 text-bold px-5 scale-100 hover:scale-[120%] min-h-[80px] min-w-[160px] transition-all ease-in duration-200 justify-center text-pretty antialiased text-lg text-white hover:bg-red-200 focus:border-8 focus:border-spacing-10 focus:border-white">
+        <button onClick={handleClick} className="rounded-3xl bg-red-300 text-bold px-5 scale-100 hover:scale-[120%] min-h-[100px] min-w-[200px] transition-all ease-in duration-200 justify-center text-pretty antialiased text-lg text-white hover:bg-red-200 focus:border-8 focus:border-spacing-10 focus:border-white">
             {children}
         </button>
     );
